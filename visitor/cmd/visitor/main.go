868ffe3da1f6961e9f85b411c73f63f380e54a0b
sshawnta <valera.loktaev@gmail.com>
2020-02-26 12:03:58 +0300

    1.1

--- visitor/cmd/visitor/main.go
+++ visitor/cmd/visitor/main.go
@@ -23,12 +23,15 @@ func main() {
 	newCar := car.NewCar(model.OpelCar, fullInfoCar)
 	//newPlane := plane.NewPlane(model.AirPlane)
 	_, _, err := newCar.FullInfo()
+	fmt.Println("price before visitor in car object")
 	if err != nil {
 		fmt.Println(err)
 	}
 	//newPlane.FullInfo()
 	newVisitor := frame.NewVisitor(newCar, model.AddPriceCar, model.SalePlane64)
 	newCar.Accept(newVisitor)
+	fmt.Println("visitor new price")
 	_, _, err = newCar.FullInfo()
+	fmt.Println("price after visitor in car object")
 	//newPlane.Accept(newVisitor)
 }
